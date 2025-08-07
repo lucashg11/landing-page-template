3fbf7001a33fc55903656dfa7bb6ee9aa37e58b3
Lucas Gomez <gomezlucashernan@hotmail.com>
2025-08-07 18:37:06 -0300

    Added counter box with animated numbers

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const topBar = document.querySelector(".topbar");
 const scrollers = document.querySelectorAll(".marquee");
+const counters = document.querySelectorAll(".counter");
+const speed = 300;
+
+
 
 function changeTopBarBg() {
     if (window.scrollY > 1) {
@@ -10,3 +14,21 @@ function changeTopBarBg() {
 }
 
 window.addEventListener("scroll", changeTopBarBg);
+
+counters.forEach(counter =>{
+    const animate = ()=>{
+        const value = +counter.getAttribute("num");
+        const data = +counter.innerText;
+
+        const time = value / speed;
+        if(data < value){
+            counter.innerText = Math.ceil(data + time);
+            setTimeout(animate, 1); 
+        }else{
+            counter.innerText = value;
+    
+        }
+    }
+    animate();
+    
+})
