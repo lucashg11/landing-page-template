22e7a6f83cc03aa7043d33a9bf7e029d4a633a04
Lucas Gomez <gomezlucashernan@hotmail.com>
2025-11-07 16:23:07 -0300

    features section, contact section

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 const topBar = document.querySelector(".topbar");
+const navLink = document.querySelectorAll(".nav-item a");
 const scrollers = document.querySelectorAll(".marquee");
 const counters = document.querySelectorAll(".counter");
 const speed = 300;
 
-
-
 function changeTopBarBg() {
-    if (window.scrollY > 1) {
-        topBar.classList.add("changeBg");
-    } else {
-        topBar.classList.remove("changeBg");
-    }
+  if (window.scrollY > 1) {
+    topBar.classList.add("changeBg");
+  } else {
+    topBar.classList.remove("changeBg");
+  }
 }
 
 window.addEventListener("scroll", changeTopBarBg);
 
-counters.forEach(counter =>{
-    const animate = ()=>{
-        const value = +counter.getAttribute("num");
-        const data = +counter.innerText;
+counters.forEach((counter) => {
+  const animate = () => {
+    const value = +counter.getAttribute("num");
+    const data = +counter.innerText;
 
-        const time = value / speed;
-        if(data < value){
-            counter.innerText = Math.ceil(data + time);
-            setTimeout(animate, 1); 
-        }else{
-            counter.innerText = value;
-    
-        }
+    const time = value / speed;
+    if (data < value) {
+      counter.innerText = Math.ceil(data + time);
+      setTimeout(animate, 1);
+    } else {
+      counter.innerText = value;
     }
-    animate();
-    
-})
+  };
+  animate();
+});
